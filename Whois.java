b4068559905fc13c81a87aace8d1efaec10ade2f
Kimberly <44447847+iamkimbert@users.noreply.github.com>
2023-10-23 22:27:45 -0400

    Use try-with-resources to close a socket

--- Whois.java
+++ Whois.java
@@ -2,29 +2,60 @@
 import java.net.*;
 import java.io.*;
 
+// class Whois {
+// 	public static void main (String args[]) throws Exception {
+// 		int c;
+
+// 		// Create a socket connected to internic.net, port 43.
+// 		Socket s = new Socket("whois.internic.net", 43);
+
+// 		// Obtain input and output streams.
+// 		InputStream in = s.getInputStream();
+// 		OutputStream out = s.getOutputStream();
+
+// 		// Construct a request string.
+// 		String str = (args.length == 0 ? "MHProfessional.com" : args[0]) + "\n";
+// 		// Convert to bytes.
+// 		byte buf[] = str.getBytes();
+
+// 		// send request.
+// 		out.write(buf);
+
+// 		// Read and display response. 
+// 		while ((c = in.read())!= -1) {
+// 			System.out.print((char) c);
+// 		}
+// 		s.close();
+// 	}
+// }
+
 class Whois {
+
+	// Use try-with-resources to close a socket. 
 	public static void main (String args[]) throws Exception {
 		int c;
 
-		// Create a socket connected to internic.net, port 43.
-		Socket s = new Socket("whois.internic.net", 43);
+		// Create a socket connected to internic.net, port 43. Manage this
+		//socket with a try-with-resources block.
+		try (Socket s = new Socket("whois.internic.net", 43)) {
 
-		// Obtain input and output streams.
-		InputStream in = s.getInputStream();
-		OutputStream out = s.getOutputStream();
+			// Obtain input and output streams.
+			InputStream in = s.getInputStream();
+			OutputStream out = s.getOutputStream();
 
-		// Construct a request string.
-		String str = (args.length == 0 ? "MHProfessional.com" : args[0]) + "\n";
-		// Convert to bytes.
-		byte buf[] = str.getBytes();
+			// Construct a request string.
+			String str = (args.length == 0 ? "MHProfessional.com" : args[0]) + "\n";
+			// Convert to bytes.
+			byte buf[] = str.getBytes();
 
-		// send request.
-		out.write(buf);
+			// Send request
+			out.write(buf);
 
-		// Read and display response. 
-		while ((c = in.read())!= -1) {
-			System.out.print((char) c);
+			// Read and display response.
+			while ((c = in.read()) != -1) {
+				System.out.print((char) c);
+			}
 		}
-		s.close();
+		// The socket is now closed 
 	}
 }
